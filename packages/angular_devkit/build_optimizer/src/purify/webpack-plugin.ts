d9a3afa3ce3d8bae417f86fcb43e764e251d4df9
Gabriel Schuster <actra-gschuster@users.noreply.github.com>
2017-09-07 13:51:46 +0200

    fix(@angular-devkit/build-optimizer): support ...
    ...disabled webpack cache

--- packages/angular_devkit/build_optimizer/src/purify/webpack-plugin.ts
+++ packages/angular_devkit/build_optimizer/src/purify/webpack-plugin.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import * as webpack from 'webpack';
-import { RawSource } from 'webpack-sources';
+import { ConcatSource, RawSource } from 'webpack-sources';
 import { purify } from './purify';
 
 
@@ -25,7 +25,7 @@ export class PurifyPlugin {
             .filter((fileName: string) => fileName.endsWith('.js'))
             .forEach((fileName: string) => {
               const purifiedSource = purify(compilation.assets[fileName].source());
-              compilation.assets[fileName] = new RawSource(purifiedSource);
+              compilation.assets[fileName] = new ConcatSource(new RawSource(purifiedSource));
             });
         });
         callback();
