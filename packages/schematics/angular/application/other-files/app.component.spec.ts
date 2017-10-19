66092c41e72fc7588e17fd75518ad947dfbd6e49
Stephen Cavaliere <stephencavaliere@gmail.com>
2017-10-19 13:42:37 -0400

    fix(@schematics/angular): update application and component spec schematics

--- packages/schematics/angular/application/other-files/app.component.spec.ts
+++ packages/schematics/angular/application/other-files/app.component.spec.ts
@@ -1,7 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';<% if (routing) { %>
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';<% if (routing) { %>
 import { RouterTestingModule } from '@angular/router/testing';<% } %>
+
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({<% if (routing) { %>
       imports: [
@@ -10,22 +15,26 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    })
+    .compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title '<%= prefix %>'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('<%= prefix %>');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have as title '<%= prefix %>'`, () => {
+    expect(component.title).toEqual('<%= prefix %>');
+  });
+
+  it('should render title in a h1 tag', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to <%= prefix %>!');
-  }));
+  });
 });
