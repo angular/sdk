e5e966d14798437886d7dc51d1af39a17a34e6c8
Carlos Roso <ce.roso398@gmail.com>
2017-12-16 21:07:24 -0500

    refactor(@schematics/angular): Alphabetized imports

--- packages/schematics/angular/utility/find-module.ts
+++ packages/schematics/angular/utility/find-module.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { Path, join, normalize, relative, dasherize } from '@angular-devkit/core';
+import { Path, dasherize, join, normalize, relative } from '@angular-devkit/core';
 import { DirEntry, Tree } from '@angular-devkit/schematics';
 
 
