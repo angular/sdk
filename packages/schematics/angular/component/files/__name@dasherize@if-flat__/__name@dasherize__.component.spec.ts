3c3757213e300ddbe0a45502683e0eefef684755
Don Denton <don@happycollision.com>
2018-04-24 15:18:30 -0500

    Create a HostComponent in new component spec
    This is how I tend to start every new component I create. I know that this should perhaps not be the default right away, but maybe to be considered at a major version bump.
    
    Thoughts are welcome. Especially since there may be a better strategy for testing components which accept `@Input` of which I am not aware.

--- packages/schematics/angular/component/files/__name@dasherize@if-flat__/__name@dasherize__.component.spec.ts
+++ packages/schematics/angular/component/files/__name@dasherize@if-flat__/__name@dasherize__.component.spec.ts
@@ -1,22 +1,38 @@
+import { Component, ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { <%= classify(name) %>Component } from './<%= dasherize(name) %>.component';
 
+@Component({
+  selector: 'app-host',
+  template: `
+    <app-<%= dasherize(name) %>></app-<%= dasherize(name) %>>
+  `,
+})
+class HostComponent {
+  @ViewChild(SimplePlaylistVideoComponent) componentUnderTest: SimplePlaylistVideoComponent;
+}
+
 describe('<%= classify(name) %>Component', () => {
   let component: <%= classify(name) %>Component;
+  let host: HostComponent;
   let fixture: ComponentFixture<<%= classify(name) %>Component>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ <%= classify(name) %>Component ]
+      declarations: [
+        <%= classify(name) %>Component,
+        HostComponent
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(<%= classify(name) %>Component);
-    component = fixture.componentInstance;
+    host = fixture.componentInstance;
     fixture.detectChanges();
+    component = host.componentUnderTest;
   });
 
   it('should create', () => {
