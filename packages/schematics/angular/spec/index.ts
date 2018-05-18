5ad8246fbca3d6164c3f5538199a9ccc91585be5
smnbbrv <simonellez@gmail.com>
2018-05-18 10:23:50 +0200

    feat(@schematics/angular): add schematic for spec files
    add schematic for spec in order to allow @angular/cli users to recreate disabled / removed spec files
    
    implement main part of https://github.com/angular/angular-cli/issues/7727

---
+++ packages/schematics/angular/spec/index.ts
@@ -0,0 +1,69 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { strings } from '@angular-devkit/core';
+import {
+  apply,
+  filter,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  template,
+  Tree,
+  url,
+} from '@angular-devkit/schematics';
+import { getWorkspace } from '../utility/config';
+import { parseName } from '../utility/parse-name';
+import { Schema as PipeOptions } from './schema';
+
+const supportedTypes = ['component', 'directive', 'guard', 'service', 'pipe', 'module'];
+
+export default function (options: PipeOptions): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    const workspace = getWorkspace(host);
+    if (!options.project) {
+      throw new SchematicsException('Option (project) is required.');
+    }
+    const project = workspace.projects[options.project];
+
+    if (options.path === undefined) {
+      const projectDirName = project.projectType === 'application' ? 'app' : 'lib';
+      options.path = `/${project.root}/src/${projectDirName}`;
+    }
+
+    const parsedPath = parseName(options.path, options.name);
+
+    let [, name, type] = parsedPath.name.replace(/\.ts$/, '').match(/(.*)\.([^.]+)$/) || [null, null, null];
+
+    if (!name || !type) {
+      throw new SchematicsException(
+        `The provided name / file should look like name.type (e.g. component-name.component) or name.type.ts (e.g. component-name.component.ts).`
+      );
+    }
+
+    if (!supportedTypes.includes(type)) {
+      throw new SchematicsException(`The type "${ type }" is not supported. Please use one of [${ supportedTypes.join(', ') }].`);
+    }
+
+    options.name = name;
+    options.path = parsedPath.path;
+
+    const templateSource = apply(url(`../${type}/files`), [
+      filter(path => path.endsWith('.spec.ts')),
+      template({
+        ...strings,
+        'if-flat': () => '',
+        ...options,
+      }),
+      move(parsedPath.path),
+    ]);
+
+    return mergeWith(templateSource)(host, context);
+  };
+}
